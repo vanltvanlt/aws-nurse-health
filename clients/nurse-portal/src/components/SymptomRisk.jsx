df1fcc9bf1fc54cb29f7b0bbe525cee961c7b0e2
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2024-12-04 12:55:39 -0500

    Revert "SymptomsRisk"
    This reverts commit 92683c60181d0d559d12b792cc9b00810be7466c.

--- clients/nurse-portal/src/components/SymptomRisk.jsx
+++ clients/nurse-portal/src/components/SymptomRisk.jsx
@@ -1,60 +1,18 @@
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 
-// const AssignPatients = ({ selectedPatient }) => {
-//   console.log(selectedPatient);
-//   return (
-//     <div>
-//       <h3>List of Syptoms</h3>
-
-//       <h3>Risk Prediction</h3>
-//     </div>
-//   );
-// };
-
-// AssignPatients.propTypes = {
-//   selectedPatient: PropTypes.string.isRequired,
-// };
-
-// export default AssignPatients;
-import React, { useEffect, useState } from "react";
-import { getSymptomsRisk } from "../services/symptomsRiskService";
-
-const SymptomsRisk = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchSymptomsRisk = async () => {
-      try {
-        const predictions = await getSymptomsRisk();
-        setData(predictions);
-      } catch (err) {
-        setError("Failed to load symptoms risk data.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchSymptomsRisk();
-  }, []);
-
-  if (loading) return <div>Loading symptoms risk...</div>;
-  if (error) return <div>{error}</div>;
-
+const AssignPatients = ({ selectedPatient }) => {
+  console.log(selectedPatient);
   return (
-    <div className="symptoms-risk-tile">
-      <h3>Patient Symptoms & Risks</h3>
-      <ul>
-        {data.map((item, index) => (
-          <li key={index}>
-            <strong>Symptoms:</strong> {item.symptoms} <br />
-            <strong>Risk:</strong> {item.risk}
-          </li>
-        ))}
-      </ul>
+    <div>
+      <h3>List of Syptoms</h3>
+
+      <h3>Risk Prediction</h3>
     </div>
   );
 };
 
-export default SymptomsRisk;
+AssignPatients.propTypes = {
+  selectedPatient: PropTypes.string.isRequired,
+};
+
+export default AssignPatients;
