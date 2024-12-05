a1563fb7a4aae6289d7bc602bb439f3b1c6a59d6
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2024-12-05 09:35:04 -0500

    Changing MongoDB to use online MongoDB Atlas

--- servers/user-portal/config/db.js
+++ servers/user-portal/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const mongoUri = "mongodb://localhost:27017/auth-service-db";
-mongoose.connect(mongoUri, {});
+// const mongoUri = "mongodb://localhost:27017/auth-service-db";
+mongoose.connect(process.env.MONGODB_URI, {});
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
